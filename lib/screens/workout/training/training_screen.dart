c486c065e65ec936912fb5c3ebbce3523d139bcc
eSaniello <shaniel29samadhan@gmail.com>
2020-01-19 19:08:21 -0300

    Active workout now shows on training screen.

--- lib/screens/workout/training/training_screen.dart
+++ lib/screens/workout/training/training_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Training extends StatefulWidget {
@@ -6,13 +7,56 @@ class Training extends StatefulWidget {
 }
 
 class _TrainingState extends State<Training> {
+  Map wp;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Firestore.instance.collection("workout_plans").getDocuments().then(
+      (QuerySnapshot snapshot) {
+        snapshot.documents.forEach((doc) {
+          try {
+            Firestore.instance
+                .collection('workout_plans')
+                .document(doc.documentID)
+                .get()
+                .then((d) {
+              if (d.data['activeWorkout'] == true) {
+                setState(() {
+                  wp = d.data;
+                });
+              } else {
+                setState(() {
+                  wp = null;
+                });
+              }
+            });
+          } catch (e) {
+            print(e.toString());
+          }
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "meow",
-        style: TextStyle(color: Colors.white),
-      ),
-    );
+    return wp == null
+        ? Text('loading')
+        : SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Current plan: ' + wp['workoutName'],
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  'Description: ' + wp['workoutDesc'],
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          );
   }
 }
