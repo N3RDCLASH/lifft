5509f5c57b2ce04bd280ff0576072e3845baa523
eSaniello <shaniel29samadhan@gmail.com>
2020-01-26 19:42:06 -0300

    working on sets list bug

--- lib/screens/workout/training/log_exercise_screen.dart
+++ lib/screens/workout/training/log_exercise_screen.dart
@@ -70,6 +70,121 @@ class _LogExerciseState extends State<LogExercise> {
     }
   }
 
+  static TextEditingController wCtrl1 = TextEditingController();
+  static TextEditingController wCtrl2 = TextEditingController();
+  static TextEditingController wCtrl3 = TextEditingController();
+
+  static TextEditingController rCtrl1 = TextEditingController();
+  static TextEditingController rCtrl2 = TextEditingController();
+  static TextEditingController rCtrl3 = TextEditingController();
+
+  List setMaps = List(100);
+
+  List<Widget> sets = [
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text('1'),
+        SizedBox(width: 20),
+        Flexible(
+          child: TextField(
+            controller: wCtrl1,
+            decoration: InputDecoration(hintText: '10'),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+        SizedBox(width: 20),
+        Flexible(
+          child: TextField(
+            controller: rCtrl1,
+            decoration: InputDecoration(hintText: '12'),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+      ],
+    ),
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text('2'),
+        SizedBox(width: 20),
+        Flexible(
+          child: TextField(
+            controller: wCtrl2,
+            decoration: InputDecoration(hintText: '15'),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+        SizedBox(width: 20),
+        Flexible(
+          child: TextField(
+            controller: rCtrl2,
+            decoration: InputDecoration(hintText: '12'),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+      ],
+    ),
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text('3'),
+        SizedBox(width: 20),
+        Flexible(
+          child: TextField(
+            controller: wCtrl3,
+            decoration: InputDecoration(hintText: '20'),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+        SizedBox(width: 20),
+        Flexible(
+          child: TextField(
+            controller: rCtrl3,
+            decoration: InputDecoration(hintText: '12'),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+      ],
+    ),
+  ];
+
+  int setCount = 3;
+  List controllers = [];
+
+  _addNewSet() {
+    setState(() {
+      setCount++;
+      TextEditingController ctr = TextEditingController();
+
+      controllers.add(ctr);
+
+      sets.add(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text('$setCount'),
+            SizedBox(width: 20),
+            Flexible(
+              child: TextField(
+                controller: ctr,
+                decoration: InputDecoration(hintText: '20'),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            SizedBox(width: 20),
+            Flexible(
+              child: TextField(
+                decoration: InputDecoration(hintText: '12'),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -92,65 +207,49 @@ class _LogExerciseState extends State<LogExercise> {
                 Text('Reps'),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('1'),
-                SizedBox(width: 20),
-                Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(hintText: '25'),
-                    keyboardType: TextInputType.number,
-                  ),
-                ),
-                SizedBox(width: 20),
-                Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(hintText: '12'),
-                    keyboardType: TextInputType.number,
-                  ),
-                ),
-              ],
+            ...sets,
+            FlatButton(
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.add),
+                  Text('new set'),
+                ],
+              ),
+              onPressed: _addNewSet,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('1'),
-                SizedBox(width: 20),
-                Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(hintText: '15'),
-                    keyboardType: TextInputType.number,
-                  ),
-                ),
-                SizedBox(width: 20),
-                Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(hintText: '12'),
-                    keyboardType: TextInputType.number,
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('1'),
-                SizedBox(width: 20),
-                Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(hintText: '20'),
-                    keyboardType: TextInputType.number,
-                  ),
-                ),
-                SizedBox(width: 20),
-                Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(hintText: '12'),
-                    keyboardType: TextInputType.number,
-                  ),
-                ),
-              ],
+            RaisedButton(
+              child: Text('Log set & rest'),
+              onPressed: () {
+                // try {
+                //   Firestore.instance.collection('logs').add({
+                //     'exercise': widget.ds['name'],
+
+                //   });
+                // } catch (e) {
+                //   print(e);
+                // }
+
+                setMaps[0] = {
+                  'set': 1,
+                  'weight': wCtrl1.text,
+                  'reps': rCtrl1.text,
+                };
+
+                setMaps[1] = {
+                  'set': 2,
+                  'weight': wCtrl2.text,
+                  'reps': rCtrl2.text,
+                };
+
+                setMaps[2] = {
+                  'set': 3,
+                  'weight': wCtrl3.text,
+                  'reps': rCtrl3.text,
+                };
+
+                // print(setMaps);
+                print(sets[0]);
+              },
             ),
           ],
         ),
