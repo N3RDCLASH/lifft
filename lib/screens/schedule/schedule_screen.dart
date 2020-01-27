9c154325ea7138a8abe9a3905484fd5043b9f453
eSaniello <shaniel29samadhan@gmail.com>
2020-01-27 15:30:44 -0300

    figured out how to put custom events on calendar days.

--- lib/screens/schedule/schedule_screen.dart
+++ lib/screens/schedule/schedule_screen.dart
@@ -7,15 +7,6 @@ class Schedule extends StatefulWidget {
 }
 
 class _ScheduleState extends State<Schedule> with TickerProviderStateMixin {
-  // Example holidays
-  // final Map<DateTime, List> _holidays = {
-  //   DateTime(2019, 1, 1): ['New Year\'s Day'],
-  //   DateTime(2019, 1, 6): ['Epiphany'],
-  //   DateTime(2019, 2, 14): ['Valentine\'s Day'],
-  //   DateTime(2019, 4, 21): ['Easter Sunday'],
-  //   DateTime(2019, 4, 22): ['Easter Monday'],
-  // };
-
   Map<DateTime, List> _events;
   List _selectedEvents;
   AnimationController _animationController;
@@ -27,58 +18,8 @@ class _ScheduleState extends State<Schedule> with TickerProviderStateMixin {
     final _selectedDay = DateTime.now();
 
     _events = {
-      _selectedDay.subtract(Duration(days: 30)): [
-        'Event A0',
-        'Event B0',
-        'Event C0'
-      ],
-      DateTime.now().add(Duration(days: 2)): ['Meow 01', 'Meow 02'],
-      _selectedDay.subtract(Duration(days: 27)): ['Event A1'],
-      _selectedDay.subtract(Duration(days: 20)): [
-        'Event A2',
-        'Event B2',
-        'Event C2',
-        'Event D2'
-      ],
-      _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3'],
-      _selectedDay.subtract(Duration(days: 10)): [
-        'Event A4',
-        'Event B4',
-        'Event C4'
-      ],
-      _selectedDay.subtract(Duration(days: 4)): [
-        'Event A5',
-        'Event B5',
-        'Event C5'
-      ],
-      _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
-      _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
-      _selectedDay.add(Duration(days: 1)): [
-        'Event A8',
-        'Event B8',
-        'Event C8',
-        'Event D8'
-      ],
-      _selectedDay.add(Duration(days: 3)):
-          Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
-      _selectedDay.add(Duration(days: 7)): [
-        'Event A10',
-        'Event B10',
-        'Event C10'
-      ],
-      _selectedDay.add(Duration(days: 11)): ['Event A11', 'Event B11'],
-      _selectedDay.add(Duration(days: 17)): [
-        'Event A12',
-        'Event B12',
-        'Event C12',
-        'Event D12'
-      ],
-      _selectedDay.add(Duration(days: 22)): ['Event A13', 'Event B13'],
-      _selectedDay.add(Duration(days: 26)): [
-        'Event A14',
-        'Event B14',
-        'Event C14'
-      ],
+      DateTime.now().add(Duration(days: 2)): ['Meow 02'],
+      DateTime.now().add(Duration(days: 1)): ['Meow 01'],
     };
 
     _selectedEvents = _events[_selectedDay] ?? [];
@@ -136,7 +77,7 @@ class _ScheduleState extends State<Schedule> with TickerProviderStateMixin {
         selectedColor: Colors.deepOrange[400],
         todayColor: Colors.blue,
         markersColor: Colors.green,
-        outsideDaysVisible: false,
+        outsideDaysVisible: true,
       ),
       headerStyle: HeaderStyle(
         formatButtonTextStyle: TextStyle(color: Colors.white),
